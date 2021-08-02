095ef411ee19badbf81eea3281a70b5fa0372250
1chiSensei <mail@tomio.codes>
2021-08-02 18:13:57 +0800

    feat: add run command

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use poise::serenity_prelude as serenity;
 use std::{env::var, time::Duration};
+use serde::Deserialize;
 
 type Error = Box<dyn std::error::Error + Send + Sync>;
 type PrefixContext<'a> = poise::PrefixContext<'a, Data, Error>;
@@ -33,6 +34,53 @@ code here
     }
 }
 
+#[derive(Deserialize)]
+struct Response {
+    code: Option<usize>,
+    language: String,
+    output: String,
+    signal: Option<String>,
+    stderr: String,
+    stdout: String,
+    version: String
+}
+
+#[derive(Deserialize)]
+struct Bin {
+    key: String
+}
+
+async fn execute_piston(ctx: PrefixContext<'_>, lang: String, code: String) -> Result<Response, Error> {
+    let params = [("lang", lang), ("code", code)];
+    let json = ctx
+        .data
+        .http
+        .post(var("API_URL")?)
+        .form(&params)
+        .send()
+        .await?
+        .json::<Response>()
+        .await?;
+
+    Ok(json)
+}
+
+async fn post_bin(ctx: PrefixContext<'_>, content: String) -> Result<String, Error> {
+    let json = ctx
+        .data
+        .http
+        .post("https://paste.nomsy.net/documents")
+        .header("Content-Type", "text/plain")
+        .body(content)
+        .send()
+        .await?
+        .json::<Bin>()
+        .await?;
+    let url = format!("https://paste.nomsy.net/{}", json.key);
+
+    Ok(url)
+}
+
 #[poise::command(track_edits, broadcast_typing)]
 async fn hello(ctx: PrefixContext<'_>) -> Result<(), Error> {
     poise::say_reply(poise::Context::Prefix(ctx), format!("Hello, {}", ctx.msg.author)).await?;
@@ -45,7 +93,7 @@ async fn help(
     ctx: PrefixContext<'_>,
     #[description = "A command to show help for."] command: Option<String>
 ) -> Result<(), Error> {
-    let bottom_text = "Type ?help command for more info on a command.
+    let bottom_text = "Type run help command for more info on a command.
 You can edit your message to the bot and the bot will edit its response.";
 
     poise::defaults::help(
@@ -58,6 +106,59 @@ You can edit your message to the bot and the bot will edit its response.";
     Ok(())
 }
 
+#[poise::command(track_edits, broadcast_typing)]
+async fn run(
+    ctx: PrefixContext<'_>,
+    code: poise::CodeBlock
+) -> Result<(), Error> {
+    match code.language {
+        None => {
+            poise::say_reply(poise::Context::Prefix(ctx), "The codeblock is missing a language...".to_string());
+
+            Ok(())
+        },
+        Some(lang) => {
+            let res = execute_piston(ctx, lang, code.code).await;
+
+            match res {
+                Err(_) => {
+                    poise::say_reply(poise::Context::Prefix(ctx), "You provided an invalid language...".to_string());
+
+                    Ok(())
+                },
+                Ok(re) => {
+                    if re.output.chars().count() > 500 {
+                        let url = post_bin(ctx, re.output).await?;
+                        let msg = format!("The response was longer than 500 characters, so I uploaded it to a paste!
+<{}>
+                        ", url);
+
+                        poise::say_reply(poise::Context::Prefix(ctx), msg);
+                        Ok(())
+                    } else if re.stderr.chars().count() > 0 {
+                        let msg = format!("An error occured!
+```sh
+{}
+```
+                        ", re.stderr);
+
+                        poise::say_reply(poise::Context::Prefix(ctx), msg);
+                        Ok(())
+                    } else {
+                        let msg = format!("```{}
+{}
+```
+                        ", *code.language.unwrap_or(re.language), re.output);
+
+                        poise::say_reply(poise::Context::Prefix(ctx), msg);
+                        Ok(())
+                    }
+                }
+            }
+        }
+    }
+}
+
 #[tokio::main]
 async fn main() -> Result<(), Error> {
     let mut options = poise::FrameworkOptions {
@@ -65,7 +166,8 @@ async fn main() -> Result<(), Error> {
             additional_prefixes: &[
                 "run,",
                 "run, ",
-                "can you run, "
+                "can you run, ",
+                "run"
             ],
             edit_tracker: Some(poise::EditTracker::for_timespan(
                 Duration::from_secs(3600 * 24 * 2),
@@ -78,9 +180,10 @@ async fn main() -> Result<(), Error> {
 
     options.command(hello(), |f| f.category("Main"));
     options.command(help(), |f| f.category("Main"));
+    options.command(run(), |f| f.category("Main"));
 
     let framework = poise::Framework::new(
-        "run".to_owned(),
+        "run ".to_owned(),
         serenity::ApplicationId(var("APPLICATION_ID")?.parse()?),
         move |_ctx, bot, _framework| {
             Box::pin(async move {
